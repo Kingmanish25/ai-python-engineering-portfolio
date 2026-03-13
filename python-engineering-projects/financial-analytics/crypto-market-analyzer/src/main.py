419c16e6169ccccac93a643bab1fe0922e8e831b
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 12:39:24 +0530

    Create main.py

---
+++ python-engineering-projects/financial-analytics/crypto-market-analyzer/src/main.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price"
+
+params = {
+    "ids": "bitcoin,ethereum",
+    "vs_currencies": "usd"
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+print(data)
