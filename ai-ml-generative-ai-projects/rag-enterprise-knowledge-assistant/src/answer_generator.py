c3ea44bd940b0c7c22d28cf162c5d016b1a5da12
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 16:13:21 +0530

    Update answer_generator.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/answer_generator.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/answer_generator.py
@@ -1,1 +1,17 @@
+def generate_answer(llm, context, query):
 
+    prompt = f"""
+    Use the context below to answer the question.
+
+    Context:
+    {context}
+
+    Question:
+    {query}
+
+    Provide source references.
+    """
+
+    response = llm(prompt)
+
+    return response
