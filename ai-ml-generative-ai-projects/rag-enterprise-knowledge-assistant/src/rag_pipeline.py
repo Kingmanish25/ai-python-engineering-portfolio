a4eebc23f5b6786892e3d6c219dbc6c0d40b8b90
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 16:13:45 +0530

    Update rag_pipeline.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/rag_pipeline.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/rag_pipeline.py
@@ -1,1 +1,12 @@
+from retriever import retrieve_context
+from answer_generator import generate_answer
 
+def run_rag(vector_db, llm, query):
+
+    docs = retrieve_context(vector_db, query)
+
+    context = "\n".join([d.page_content for d in docs])
+
+    answer = generate_answer(llm, context, query)
+
+    return answer
