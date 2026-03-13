422a388e5e6829f21b41bae8312e6cb1d28d50fb
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 16:11:05 +0530

    Update embeddings.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/embeddings.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/embeddings.py
@@ -1,1 +1,9 @@
+from langchain.embeddings import HuggingFaceEmbeddings
 
+def load_embeddings():
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    return embeddings
