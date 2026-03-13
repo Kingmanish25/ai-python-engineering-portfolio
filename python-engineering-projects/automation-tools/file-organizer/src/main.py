32e9d2aa15d6e7a627ec04bfd10326c75ae73f81
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 10:41:37 +0530

    Update main.py

--- python-engineering-projects/automation-tools/file-organizer/src/main.py
+++ python-engineering-projects/automation-tools/file-organizer/src/main.py
@@ -1,1 +1,32 @@
+import os
+import shutil
 
+def organize_files(folder_path):
+
+    for filename in os.listdir(folder_path):
+
+        file_path = os.path.join(folder_path, filename)
+
+        if os.path.isfile(file_path):
+
+            extension = filename.split(".")[-1]
+
+            target_folder = os.path.join(folder_path, extension)
+
+            if not os.path.exists(target_folder):
+                os.makedirs(target_folder)
+
+            shutil.move(file_path, os.path.join(target_folder, filename))
+
+
+def main():
+
+    folder = "downloads"
+
+    organize_files(folder)
+
+    print("Files organized successfully")
+
+
+if __name__ == "__main__":
+    main()
