08434a92782dc023314c30eaa5d74d06d5cec1ba
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-13 16:11:29 +0530

    Update vector_store.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/vector_store.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/vector_store.py
@@ -1,1 +1,10 @@
+from langchain.vectorstores import FAISS
 
+def build_vector_index(chunks, embeddings):
+
+    vector_db = FAISS.from_documents(
+        chunks,
+        embeddings
+    )
+
+    return vector_db
