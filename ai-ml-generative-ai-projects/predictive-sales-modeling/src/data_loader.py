9053dbbdfc80e33f402d3fc6c8b44ad6573b42a8
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:39:03 +0530

    Update data_loader.py

--- ai-ml-generative-ai-projects/predictive-sales-modeling/src/data_loader.py
+++ ai-ml-generative-ai-projects/predictive-sales-modeling/src/data_loader.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+
+def load_data(path="../data/retail_sales.csv"):
+
+    df = pd.read_csv(path)
+
+    df["date"] = pd.to_datetime(df["date"])
+
+    df = df.sort_values("date")
+
+    return df
+
+
+def train_test_split_time(df, test_size=30):
+
+    train = df.iloc[:-test_size]
+
+    test = df.iloc[-test_size:]
+
+    return train, test
