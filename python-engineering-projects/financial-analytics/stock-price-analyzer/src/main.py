6c57335ee69e9bc99a6f0fb66814a0e05d229d68
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 23:40:05 +0530

    Update main.py

--- python-engineering-projects/financial-analytics/stock-price-analyzer/src/main.py
+++ python-engineering-projects/financial-analytics/stock-price-analyzer/src/main.py
@@ -1,12 +1,60 @@
+import argparse
+import logging
 import yfinance as yf
 
-def analyze_stock():
 
-    ticker = yf.Ticker("AAPL")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-    data = ticker.history(period="1mo")
 
-    print(data.tail())
+def analyze_stock(ticker, period):
+
+    logging.info(f"Downloading stock data for {ticker}")
+
+    stock = yf.Ticker(ticker)
+
+    data = stock.history(period=period)
+
+    if data.empty:
+        logging.error("No market data retrieved")
+        return
+
+    latest_close = data["Close"].iloc[-1].item()
+    highest_price = data["High"].max().item()
+    lowest_price = data["Low"].min().item()
+    avg_price = data["Close"].mean().item()
+
+    print("\nStock Price Analysis:\n")
+
+    print(f"Ticker: {ticker}")
+    print(f"Latest Closing Price: {latest_close:.2f}")
+    print(f"Highest Price ({period}): {highest_price:.2f}")
+    print(f"Lowest Price ({period}): {lowest_price:.2f}")
+    print(f"Average Price ({period}): {avg_price:.2f}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Stock Price Analyzer")
+
+    parser.add_argument(
+        "--ticker",
+        default="AAPL",
+        help="Stock ticker symbol (default: AAPL)"
+    )
+
+    parser.add_argument(
+        "--period",
+        default="1mo",
+        help="Market data period (default: 1mo)"
+    )
+
+    args = parser.parse_args()
+
+    analyze_stock(args.ticker, args.period)
+
 
 if __name__ == "__main__":
-    analyze_stock()
+    main()
