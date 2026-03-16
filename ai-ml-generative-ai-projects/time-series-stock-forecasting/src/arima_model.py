809f93050d513a682ac18827960140a1931c6f28
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:18:26 +0530

    Update arima_model.py

--- ai-ml-generative-ai-projects/time-series-stock-forecasting/src/arima_model.py
+++ ai-ml-generative-ai-projects/time-series-stock-forecasting/src/arima_model.py
@@ -1,1 +1,17 @@
+from statsmodels.tsa.arima.model import ARIMA
 
+
+def train_arima(series, order=(5, 1, 0)):
+
+    model = ARIMA(series, order=order)
+
+    model_fit = model.fit()
+
+    return model_fit
+
+
+def forecast_arima(model, steps=30):
+
+    forecast = model.forecast(steps=steps)
+
+    return forecast
