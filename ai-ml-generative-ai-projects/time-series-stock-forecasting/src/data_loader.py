9402b31d7f10b194d840048273f24ec5bca49c00
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:17:55 +0530

    Update data_loader.py

--- ai-ml-generative-ai-projects/time-series-stock-forecasting/src/data_loader.py
+++ ai-ml-generative-ai-projects/time-series-stock-forecasting/src/data_loader.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def load_data(path="../data/data.csv"):
+
+    df = pd.read_csv(path)
+
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    df = df.sort_values("Date")
+
+    df.set_index("Date", inplace=True)
+
+    return df
