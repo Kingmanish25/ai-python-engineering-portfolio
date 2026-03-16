6afc696951a1bbcdbdf4d21264f529deea417761
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 15:35:25 +0530

    Update main.py

--- python-engineering-projects/automation-tools/system-resource-monitor/src/main.py
+++ python-engineering-projects/automation-tools/system-resource-monitor/src/main.py
@@ -1,12 +1,52 @@
 import psutil
+import time
+import argparse
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def monitor():
 
-    cpu = psutil.cpu_percent()
+    cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
 
-    print(f"CPU Usage: {cpu}%")
-    print(f"Memory Usage: {memory}%")
+    logging.info(f"CPU Usage: {cpu}%")
+    logging.info(f"Memory Usage: {memory}%")
+
+
+def start_monitor(interval):
+
+    logging.info(f"System monitoring started (interval: {interval} seconds)")
+
+    try:
+        while True:
+            monitor()
+            time.sleep(interval)
+
+    except KeyboardInterrupt:
+        logging.info("System monitoring stopped by user")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="System Resource Monitor")
+
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=5,
+        help="Monitoring interval in seconds"
+    )
+
+    args = parser.parse_args()
+
+    start_monitor(args.interval)
+
 
 if __name__ == "__main__":
-    monitor()
+    main()
