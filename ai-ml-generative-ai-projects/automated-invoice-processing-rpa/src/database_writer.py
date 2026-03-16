9f88096b4db21aa5b588c6ce332d3cf9448cfe2e
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:02:32 +0530

    Update database_writer.py

--- ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/database_writer.py
+++ ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/database_writer.py
@@ -1,1 +1,35 @@
+import sqlite3
 
+
+DB_FILE = "invoices.db"
+
+
+def save_invoice(invoice):
+
+    conn = sqlite3.connect(DB_FILE)
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS invoices(
+            invoice_number TEXT,
+            vendor TEXT,
+            amount REAL
+        )
+        """
+    )
+
+    cursor.execute(
+        "INSERT INTO invoices VALUES (?, ?, ?)",
+        (
+            invoice["invoice_number"],
+            invoice["vendor"],
+            invoice["amount"]
+        )
+    )
+
+    conn.commit()
+    conn.close()
+
+    print("Saved invoice:", invoice["invoice_number"])
