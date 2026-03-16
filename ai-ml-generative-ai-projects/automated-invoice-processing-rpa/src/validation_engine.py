a36c45cc9afb713ce08d9bdb3055947695b7aa36
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:14:54 +0530

    Update validation_engine.py

--- ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/validation_engine.py
+++ ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/validation_engine.py
@@ -1,12 +1,43 @@
-def validate_invoice(data):
+import sqlite3
 
-    if not data["invoice_number"]:
-        return False
+DB = "invoices.db"
 
-    if not data["supplier"]:
-        return False
 
-    if not data["total_amount"]:
-        return False
+def save_invoice(data):
 
-    return True
+    conn = sqlite3.connect(DB)
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS invoices(
+            invoice_number TEXT,
+            supplier TEXT,
+            invoice_date TEXT,
+            buyer TEXT,
+            total_amount REAL,
+            tax REAL
+        )
+        """
+    )
+
+    cursor.execute(
+        """
+        INSERT INTO invoices VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (
+            data["invoice_number"],
+            data["supplier"],
+            data["invoice_date"],
+            data["buyer"],
+            data["total_amount"],
+            data["tax"]
+        )
+    )
+
+    conn.commit()
+
+    conn.close()
+
+    print("Saved invoice:", data["invoice_number"])
