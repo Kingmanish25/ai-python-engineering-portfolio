66d44971b3fb800f1f1ead51122b32d6d5063d0d
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:41:49 +0530

    Update generator.py

--- ai-ml-generative-ai-projects/gan-synthetic-data-generator/src/generator.py
+++ ai-ml-generative-ai-projects/gan-synthetic-data-generator/src/generator.py
@@ -1,1 +1,32 @@
+import torch
+import torch.nn as nn
 
+
+class Generator(nn.Module):
+    """
+    Generator network
+    Converts random noise into synthetic tabular samples
+    """
+
+    def __init__(self, noise_dim, output_dim):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(noise_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(256, output_dim),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        return self.model(z)
