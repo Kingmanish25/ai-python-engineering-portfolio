dd759dbcf233d18d9d9f0368bfa0044fde1cd30f
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 00:58:57 +0530

    Update indicators.py

--- ai-ml-generative-ai-projects/advanced-stock-forecasting/src/indicators.py
+++ ai-ml-generative-ai-projects/advanced-stock-forecasting/src/indicators.py
@@ -1,1 +1,27 @@
+import pandas as pd
 
+
+def compute_rsi(series, period=14):
+
+    delta = series.diff()
+
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+
+    rs = gain / loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
+
+
+def compute_macd(series):
+
+    ema12 = series.ewm(span=12, adjust=False).mean()
+    ema26 = series.ewm(span=26, adjust=False).mean()
+
+    macd = ema12 - ema26
+
+    signal = macd.ewm(span=9, adjust=False).mean()
+
+    return macd, signal
