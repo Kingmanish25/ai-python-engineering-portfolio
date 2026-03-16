b9ddef67bbf43432e19a499c31b30191fcbacbaa
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:11:49 +0530

    Update config_manager.py

--- ai-ml-generative-ai-projects/network-automation-vendor-management/src/config_manager.py
+++ ai-ml-generative-ai-projects/network-automation-vendor-management/src/config_manager.py
@@ -1,1 +1,26 @@
+def push_configuration(connection, vendor):
 
+    print("Applying configuration...")
+
+    if vendor == "cisco":
+
+        commands = [
+            "interface loopback0",
+            "description Automation-Test",
+            "ip address 10.10.10.1 255.255.255.255"
+        ]
+
+    elif vendor == "juniper":
+
+        commands = [
+            "set interfaces lo0 unit 0 family inet address 10.10.10.1/32"
+        ]
+
+    else:
+
+        print("Unsupported vendor")
+        return
+
+    output = connection.send_config_set(commands)
+
+    print(output)
