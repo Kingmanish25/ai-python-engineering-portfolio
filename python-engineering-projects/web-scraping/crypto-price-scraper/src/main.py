b1a842e0452ba4833532aabecc3de19daccbd1e9
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 00:00:40 +0530

    Update main.py

--- python-engineering-projects/web-scraping/crypto-price-scraper/src/main.py
+++ python-engineering-projects/web-scraping/crypto-price-scraper/src/main.py
@@ -1,16 +1,49 @@
+import argparse
+import logging
 import requests
 
-URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-def get_crypto_price():
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-    response = requests.get(URL)
+API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-    data = response.json()
 
-    price = data["bpi"]["USD"]["rate"]
+def get_crypto_price(currency):
 
-    print("Bitcoin Price (USD):", price)
+    logging.info("Fetching Bitcoin price from CoinDesk API")
+
+    try:
+        response = requests.get(API_URL, timeout=10)
+        response.raise_for_status()
+
+        data = response.json()
+
+        price = data["bpi"][currency]["rate"]
+
+        print("\nCrypto Price:\n")
+        print(f"Bitcoin Price ({currency}): {price}")
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to retrieve crypto price: {e}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Crypto Price Scraper")
+
+    parser.add_argument(
+        "--currency",
+        default="USD",
+        help="Currency to display Bitcoin price (default: USD)"
+    )
+
+    args = parser.parse_args()
+
+    get_crypto_price(args.currency)
+
 
 if __name__ == "__main__":
-    get_crypto_price()
+    main()
