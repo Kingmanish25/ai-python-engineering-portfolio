829db6d2b8b2ce5ccc3e359749eeb4a65161b110
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 15:01:48 +0530

    Update main.py

--- python-engineering-projects/automation-tools/file-organizer/src/main.py
+++ python-engineering-projects/automation-tools/file-organizer/src/main.py
@@ -1,7 +1,19 @@
 import os
 import shutil
+import argparse
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def organize_files(folder_path):
+
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"Folder does not exist: {folder_path}")
 
     for filename in os.listdir(folder_path):
 
@@ -9,23 +21,37 @@ def organize_files(folder_path):
 
         if os.path.isfile(file_path):
 
-            extension = filename.split(".")[-1]
+            name, extension = os.path.splitext(filename)
+
+            if extension == "":
+                extension = "no_extension"
+            else:
+                extension = extension.replace(".", "")
 
             target_folder = os.path.join(folder_path, extension)
 
-            if not os.path.exists(target_folder):
-                os.makedirs(target_folder)
+            os.makedirs(target_folder, exist_ok=True)
 
             shutil.move(file_path, os.path.join(target_folder, filename))
+
+            logging.info(f"Moved {filename} → {extension}/")
 
 
 def main():
 
-    folder = "downloads"
+    parser = argparse.ArgumentParser(description="File Organizer Tool")
 
-    organize_files(folder)
+    parser.add_argument(
+        "--folder",
+        required=True,
+        help="Folder path containing files to organize"
+    )
 
-    print("Files organized successfully")
+    args = parser.parse_args()
+
+    organize_files(args.folder)
+
+    logging.info("Files organized successfully")
 
 
 if __name__ == "__main__":
