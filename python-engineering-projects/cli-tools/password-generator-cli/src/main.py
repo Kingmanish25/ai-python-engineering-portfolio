1849e173eccb73b72093862c23ae3f00b4328566
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 16:23:43 +0530

    Update main.py

--- python-engineering-projects/cli-tools/password-generator-cli/src/main.py
+++ python-engineering-projects/cli-tools/password-generator-cli/src/main.py
@@ -1,8 +1,19 @@
 import argparse
 import random
 import string
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def generate_password(length):
+
+    if length < 6:
+        raise ValueError("Password length should be at least 6 characters")
 
     characters = string.ascii_letters + string.digits + string.punctuation
 
@@ -13,15 +24,22 @@ def generate_password(length):
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Generate secure password")
+    parser = argparse.ArgumentParser(
+        description="Password Generator CLI Tool"
+    )
 
-    parser.add_argument("--length", type=int, default=12)
+    parser.add_argument(
+        "--length",
+        type=int,
+        default=12,
+        help="Length of the generated password"
+    )
 
     args = parser.parse_args()
 
     password = generate_password(args.length)
 
-    print("Generated Password:", password)
+    logging.info(f"Generated password: {password}")
 
 
 if __name__ == "__main__":
