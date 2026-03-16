2d8b90b5a41c893d8535cf6c3015920302c928a6
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 00:39:31 +0530

    Update data_loader.py

--- ai-ml-generative-ai-projects/advanced-stock-forecasting/src/data_loader.py
+++ ai-ml-generative-ai-projects/advanced-stock-forecasting/src/data_loader.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+
+def load_data(path="data/data.csv"):
+    """
+    Load and preprocess stock dataset
+    """
+
+    df = pd.read_csv(path)
+
+    df.rename(columns={"Adj Close": "Adj_Close"}, inplace=True)
+
+    df["Date"] = pd.to_datetime(df["Date"], format="%d-%m-%Y")
+
+    df = df.sort_values("Date")
+
+    df.set_index("Date", inplace=True)
+
+    return df
