f5e3fc46089fd86f337b8e4af7b54771f7ed36c7
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 00:59:58 +0530

    Update hyperparameter_tuning.py

--- ai-ml-generative-ai-projects/advanced-stock-forecasting/src/hyperparameter_tuning.py
+++ ai-ml-generative-ai-projects/advanced-stock-forecasting/src/hyperparameter_tuning.py
@@ -1,1 +1,31 @@
+from itertools import product
+from statsmodels.tsa.arima.model import ARIMA
+import numpy as np
 
+
+def tune_arima(series):
+
+    p = d = q = range(0, 3)
+
+    best_score = float("inf")
+    best_order = None
+
+    for order in product(p, d, q):
+
+        try:
+
+            model = ARIMA(series, order=order)
+
+            results = model.fit()
+
+            aic = results.aic
+
+            if aic < best_score:
+
+                best_score = aic
+                best_order = order
+
+        except:
+            continue
+
+    return best_order
