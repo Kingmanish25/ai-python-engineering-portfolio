96074c6933a966998b8b2393f091fc0d62076d8c
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 13:57:15 +0530

    Update main.py

--- python-engineering-projects/automation-tools/automated-backup-system/src/main.py
+++ python-engineering-projects/automation-tools/automated-backup-system/src/main.py
@@ -1,21 +1,52 @@
 import os
 import shutil
+import argparse
+import logging
 from datetime import datetime
 
-SOURCE_FOLDER = "data"
-BACKUP_FOLDER = "backup"
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-def backup_files():
+def validate_source(source):
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"Source folder does not exist: {source}")
+
+
+def create_backup(source, backup_root):
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    destination = os.path.join(backup_root, f"backup_{timestamp}")
 
-    destination = os.path.join(BACKUP_FOLDER, f"backup_{timestamp}")
+    os.makedirs(backup_root, exist_ok=True)
 
-    shutil.copytree(SOURCE_FOLDER, destination)
+    shutil.copytree(source, destination)
 
-    print("Backup created:", destination)
+    logging.info(f"Backup created successfully: {destination}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Automated Folder Backup System")
+
+    parser.add_argument(
+        "--source",
+        required=True,
+        help="Source folder to backup"
+    )
+
+    parser.add_argument(
+        "--destination",
+        required=True,
+        help="Backup folder location"
+    )
+
+    args = parser.parse_args()
+
+    validate_source(args.source)
+
+    create_backup(args.source, args.destination)
 
 
 if __name__ == "__main__":
-    backup_files()
+    main()
