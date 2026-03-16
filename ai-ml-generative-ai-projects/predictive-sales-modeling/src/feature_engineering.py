f6b942e4324186c1f282d0cd654791dab84a315e
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:39:37 +0530

    Update feature_engineering.py

--- ai-ml-generative-ai-projects/predictive-sales-modeling/src/feature_engineering.py
+++ ai-ml-generative-ai-projects/predictive-sales-modeling/src/feature_engineering.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+
+def create_features(df):
+
+    df["day_of_week"] = df["date"].dt.dayofweek
+    df["month"] = df["date"].dt.month
+
+    df["lag_1"] = df["sales"].shift(1)
+    df["lag_7"] = df["sales"].shift(7)
+    df["lag_14"] = df["sales"].shift(14)
+
+    df["rolling_mean_7"] = df["sales"].rolling(7).mean()
+
+    df = df.dropna()
+
+    return df
