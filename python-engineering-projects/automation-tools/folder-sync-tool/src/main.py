c4fd1ab37cf24a01ccc0483b94c9a909a588612c
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 15:24:14 +0530

    Update main.py

--- python-engineering-projects/automation-tools/folder-sync-tool/src/main.py
+++ python-engineering-projects/automation-tools/folder-sync-tool/src/main.py
@@ -1,7 +1,24 @@
 import os
 import shutil
+import argparse
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def validate_folder(path, name):
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"{name} folder does not exist: {path}")
+
 
 def sync_folders(source, destination):
+
+    os.makedirs(destination, exist_ok=True)
 
     for file in os.listdir(source):
 
@@ -9,10 +26,36 @@ def sync_folders(source, destination):
         dst_file = os.path.join(destination, file)
 
         if os.path.isfile(src_file):
+
             shutil.copy2(src_file, dst_file)
 
-    print("Folders synchronized")
+            logging.info(f"Copied {file} → {destination}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Folder Sync Tool")
+
+    parser.add_argument(
+        "--source",
+        required=True,
+        help="Source folder path"
+    )
+
+    parser.add_argument(
+        "--destination",
+        required=True,
+        help="Destination folder path"
+    )
+
+    args = parser.parse_args()
+
+    validate_folder(args.source, "Source")
+
+    sync_folders(args.source, args.destination)
+
+    logging.info("Folder synchronization completed")
+
 
 if __name__ == "__main__":
-
-    sync_folders("source_folder", "destination_folder")
+    main()
