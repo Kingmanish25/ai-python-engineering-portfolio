a9d5e9c297a7766edbf0aef0d3ed9db82046ca09
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 16:16:56 +0530

    Update main.py

--- python-engineering-projects/cli-tools/log-analyzer-cli/src/main.py
+++ python-engineering-projects/cli-tools/log-analyzer-cli/src/main.py
@@ -1,7 +1,41 @@
-with open("log.txt") as f:
+import argparse
+import logging
+import os
 
-    lines = f.readlines()
 
-error_count = sum(1 for line in lines if "ERROR" in line)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-print("Error count:", error_count)
+
+def analyze_log(file_path):
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Log file not found: {file_path}")
+
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    error_count = sum(1 for line in lines if "ERROR" in line)
+
+    logging.info(f"Total ERROR entries: {error_count}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Log Analyzer CLI Tool")
+
+    parser.add_argument(
+        "--file",
+        required=True,
+        help="Path to log file"
+    )
+
+    args = parser.parse_args()
+
+    analyze_log(args.file)
+
+
+if __name__ == "__main__":
+    main()
