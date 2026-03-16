7d53474b671112f5820c22f1a467e1da34856acb
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:42:28 +0530

    Update discriminator.py

--- ai-ml-generative-ai-projects/gan-synthetic-data-generator/src/discriminator.py
+++ ai-ml-generative-ai-projects/gan-synthetic-data-generator/src/discriminator.py
@@ -1,1 +1,31 @@
+import torch
+import torch.nn as nn
 
+
+class Discriminator(nn.Module):
+    """
+    Discriminator network
+    Determines whether samples are real or synthetic
+    """
+
+    def __init__(self, input_dim):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+
+            nn.Linear(256, 128),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.3),
+
+            nn.Linear(128, 64),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
