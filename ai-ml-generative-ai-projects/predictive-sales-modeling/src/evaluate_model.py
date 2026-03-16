f54d3bce36f70118399085c7de5d8228a88027d1
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:40:43 +0530

    Update evaluate_model.py

--- ai-ml-generative-ai-projects/predictive-sales-modeling/src/evaluate_model.py
+++ ai-ml-generative-ai-projects/predictive-sales-modeling/src/evaluate_model.py
@@ -1,1 +1,19 @@
+import numpy as np
 
+
+def mae(y_true, y_pred):
+
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def rmse(y_true, y_pred):
+
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+
+def evaluate(y_true, y_pred):
+
+    return {
+        "MAE": mae(y_true, y_pred),
+        "RMSE": rmse(y_true, y_pred)
+    }
