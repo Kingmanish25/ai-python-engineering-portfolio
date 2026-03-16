55f3d80d2c253bb128b22167ed6e97a19ac38420
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:11:18 +0530

    Update device_connector.py

--- ai-ml-generative-ai-projects/network-automation-vendor-management/src/device_connector.py
+++ ai-ml-generative-ai-projects/network-automation-vendor-management/src/device_connector.py
@@ -1,1 +1,29 @@
+from netmiko import ConnectHandler
 
+
+USERNAME = "admin"
+PASSWORD = "admin123"
+
+
+def connect_to_device(host):
+
+    device = {
+        "device_type": "cisco_ios",
+        "host": host,
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+
+    try:
+
+        connection = ConnectHandler(**device)
+
+        print("Connection successful")
+
+        return connection
+
+    except Exception as e:
+
+        print("Connection error:", e)
+
+        return None
