e1235b0ab61acdce9f4895f0833f8eea9f4d5e61
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:02:03 +0530

    Update validation_engine.py

--- ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/validation_engine.py
+++ ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/validation_engine.py
@@ -1,1 +1,11 @@
+def validate_invoice(invoice):
 
+    if invoice["invoice_number"] == "UNKNOWN":
+        print("Invalid invoice number")
+        return False
+
+    if invoice["amount"] <= 0:
+        print("Invalid invoice amount")
+        return False
+
+    return True
