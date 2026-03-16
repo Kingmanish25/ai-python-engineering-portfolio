4b05f98ab92017a061bfb3b86e133cef34be90dd
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 04:29:40 +0530

    Update email_parser.py

--- ai-ml-generative-ai-projects/intelligent-rpa-invoice-automation/src/email_parser.py
+++ ai-ml-generative-ai-projects/intelligent-rpa-invoice-automation/src/email_parser.py
@@ -1,1 +1,29 @@
+import os
+import email
+from email import policy
+from email.parser import BytesParser
 
+
+def parse_email(file_path, attachment_dir="../data"):
+
+    with open(file_path, "rb") as f:
+        msg = BytesParser(policy=policy.default).parse(f)
+
+    attachments = []
+
+    for part in msg.iter_attachments():
+
+        filename = part.get_filename()
+
+        if filename:
+
+            filepath = os.path.join(attachment_dir, filename)
+
+            with open(filepath, "wb") as f:
+                f.write(part.get_payload(decode=True))
+
+            attachments.append(filepath)
+
+    print("Attachments extracted:", attachments)
+
+    return attachments
