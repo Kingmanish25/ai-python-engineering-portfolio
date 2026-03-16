2c5757ce5ed0688534ac183f3bda22390af9669a
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 00:59:26 +0530

    Update visualization.py

--- ai-ml-generative-ai-projects/advanced-stock-forecasting/src/visualization.py
+++ ai-ml-generative-ai-projects/advanced-stock-forecasting/src/visualization.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
 
+
+def plot_forecast(dates, actual, predicted):
+
+    plt.figure(figsize=(12,6))
+
+    plt.plot(dates, actual, label="Actual Price")
+
+    plt.plot(dates[-len(predicted):], predicted, label="Forecast")
+
+    plt.title("Stock Price Forecast")
+
+    plt.xlabel("Date")
+
+    plt.ylabel("Price")
+
+    plt.legend()
+
+    plt.grid(True)
+
+    plt.show()
