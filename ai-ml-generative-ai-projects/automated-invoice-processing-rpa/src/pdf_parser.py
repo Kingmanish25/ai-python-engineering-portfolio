19ed3796b40af140e7da143b6d46a6ed79255ca8
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 02:13:05 +0530

    Update pdf_parser.py

--- ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/pdf_parser.py
+++ ai-ml-generative-ai-projects/automated-invoice-processing-rpa/src/pdf_parser.py
@@ -1,53 +1,17 @@
 import pdfplumber
-import re
 
 
-def parse_invoice(file_path):
+def extract_text(file_path):
 
     text = ""
 
     with pdfplumber.open(file_path) as pdf:
+
         for page in pdf.pages:
-            extracted = page.extract_text()
-            if extracted:
-                text += extracted + "\n"
 
-    invoice_number = extract_invoice_number(text)
-    supplier = extract_supplier(text)
-    total_amount = extract_total(text)
+            page_text = page.extract_text()
 
-    return {
-        "invoice_number": invoice_number,
-        "vendor": supplier,
-        "amount": total_amount
-    }
+            if page_text:
+                text += page_text + "\n"
 
-
-def extract_invoice_number(text):
-
-    match = re.search(r"INVOICE\s+(INV-\d+)", text)
-
-    if match:
-        return match.group(1)
-
-    return "UNKNOWN"
-
-
-def extract_supplier(text):
-
-    match = re.search(r"Supplier:\s*(.*)", text)
-
-    if match:
-        return match.group(1).strip()
-
-    return "UNKNOWN"
-
-
-def extract_total(text):
-
-    match = re.search(r"Total Amount:\s*([0-9.]+)", text)
-
-    if match:
-        return float(match.group(1))
-
-    return 0.0
+    return text
