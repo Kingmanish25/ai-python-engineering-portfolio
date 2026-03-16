440f4e108838e6c6b47545660b70450a600d70a1
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 21:02:13 +0530

    Update main.py

--- python-engineering-projects/cli-tools/weather-cli-tool/src/main.py
+++ python-engineering-projects/cli-tools/weather-cli-tool/src/main.py
@@ -1,9 +1,45 @@
+import argparse
+import logging
 import requests
 
-city = input("Enter city: ")
 
-url = f"https://wttr.in/{city}?format=3"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-response = requests.get(url)
 
-print(response.text)
+def get_weather(city):
+
+    url = f"https://wttr.in/{city}?format=3"
+
+    try:
+        response = requests.get(url, timeout=5)
+
+        if response.status_code == 200:
+            logging.info(f"Weather data for {city}")
+            print(response.text)
+        else:
+            logging.error(f"Failed to retrieve weather data (status code {response.status_code})")
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Network error occurred: {e}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Weather CLI Tool")
+
+    parser.add_argument(
+        "--city",
+        required=True,
+        help="City name to fetch weather information"
+    )
+
+    args = parser.parse_args()
+
+    get_weather(args.city)
+
+
+if __name__ == "__main__":
+    main()
