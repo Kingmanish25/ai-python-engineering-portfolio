ce077768998d78fe6ad8653fb2dcc8e9221ee814
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-16 20:54:49 +0530

    Update main.py

--- python-engineering-projects/cli-tools/url-status-checker-cli/src/main.py
+++ python-engineering-projects/cli-tools/url-status-checker-cli/src/main.py
@@ -1,7 +1,40 @@
+import argparse
+import logging
 import requests
 
-url = input("Enter URL: ")
 
-response = requests.get(url)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-print("Status Code:", response.status_code)
+
+def check_url(url):
+
+    try:
+        response = requests.get(url, timeout=5)
+
+        logging.info(f"URL checked: {url}")
+        logging.info(f"Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Failed to reach URL: {e}")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="URL Status Checker CLI Tool")
+
+    parser.add_argument(
+        "--url",
+        required=True,
+        help="URL to check"
+    )
+
+    args = parser.parse_args()
+
+    check_url(args.url)
+
+
+if __name__ == "__main__":
+    main()
