c729fcc5cd1f450a25b219cb8e778c1d38d66dd6
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:19:01 +0530

    Update prophet_model.py

--- ai-ml-generative-ai-projects/time-series-stock-forecasting/src/prophet_model.py
+++ ai-ml-generative-ai-projects/time-series-stock-forecasting/src/prophet_model.py
@@ -1,1 +1,24 @@
+import pandas as pd
+from prophet import Prophet
 
+
+def train_prophet(df):
+
+    data = df.reset_index()[["Date", "Close"]]
+
+    data.columns = ["ds", "y"]
+
+    model = Prophet()
+
+    model.fit(data)
+
+    return model
+
+
+def forecast_prophet(model, periods=30):
+
+    future = model.make_future_dataframe(periods=periods)
+
+    forecast = model.predict(future)
+
+    return forecast
