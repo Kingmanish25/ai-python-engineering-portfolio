3b0a30d8116f05563c6207ed0d04c5606195ad09
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 03:12:20 +0530

    Update monitoring.py

--- ai-ml-generative-ai-projects/network-automation-vendor-management/src/monitoring.py
+++ ai-ml-generative-ai-projects/network-automation-vendor-management/src/monitoring.py
@@ -1,1 +1,16 @@
 
+def monitor_device(connection):
+
+    print("Running monitoring checks...")
+
+    commands = [
+        "show ip interface brief",
+        "show version"
+    ]
+
+    for cmd in commands:
+
+        output = connection.send_command(cmd)
+
+        print("\nCommand:", cmd)
+        print(output[:500])
