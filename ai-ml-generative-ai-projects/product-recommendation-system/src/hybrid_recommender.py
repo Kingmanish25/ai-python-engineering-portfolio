e2f5bea7c2f3cbf38ae1e7f72fa02d3abda2b9a3
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:22:36 +0530

    Update hybrid_recommender.py

--- ai-ml-generative-ai-projects/product-recommendation-system/src/hybrid_recommender.py
+++ ai-ml-generative-ai-projects/product-recommendation-system/src/hybrid_recommender.py
@@ -1,1 +1,18 @@
+class HybridRecommender:
 
+    def __init__(self, content_model, collab_model):
+
+        self.content_model = content_model
+        self.collab_model = collab_model
+
+    def recommend(self, user_id, product_id):
+
+        content_rec = self.content_model.recommend(product_id)
+
+        collab_rec = self.collab_model.recommend(user_id)
+
+        print("\nContent-Based Recommendations:")
+        print(content_rec)
+
+        print("\nCollaborative Filtering Recommendations:")
+        print(collab_rec)
