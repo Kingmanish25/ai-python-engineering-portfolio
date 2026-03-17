b2860329fcf27a5ff51cfeceeec5bd67abb511f8
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:54:38 +0530

    Update feature_engineering.py

--- ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/feature_engineering.py
+++ ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/feature_engineering.py
@@ -1,1 +1,19 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
+
+def prepare_features(df):
+
+    X = df.drop(columns=["Exited", "CustomerId"])
+    y = df["Exited"]
+
+    scaler = StandardScaler()
+
+    X_scaled = scaler.fit_transform(X)
+
+    return X_scaled, y
+
+
+def split_data(X, y):
+
+    return train_test_split(X, y, test_size=0.2, random_state=42)
