20dd8e7539758dbb56bb47f3455fb8a1b748c29d
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 17:49:33 +0530

    Update retriever.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/retriever.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/retriever.py
@@ -1,5 +1,20 @@
-def retrieve_context(vector_db, query):
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from vector_store import load_vectorstore
+from llm_inference import get_llm
+from metadata_filter import build_filter
 
-    docs = vector_db.similarity_search(query, k=4)
+def get_retriever(query, year=None, month=None):
+    db = load_vectorstore()
+    llm = get_llm()
 
-    return docs
+    base = db.as_retriever(
+        search_kwargs={
+            "k": 20,
+            "filter": build_filter(year, month)
+        }
+    )
+
+    return MultiQueryRetriever.from_llm(
+        retriever=base,
+        llm=llm
+    )
