f4c0d224b444e1f496f9fd770707a542204bab40
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:20:45 +0530

    Update data_loader.py

--- ai-ml-generative-ai-projects/product-recommendation-system/src/data_loader.py
+++ ai-ml-generative-ai-projects/product-recommendation-system/src/data_loader.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def load_data(path):
+
+    df = pd.read_csv(path)
+
+    # Convert datetime
+    df["purchase_time"] = pd.to_datetime(df["purchase_time"])
+
+    # Basic cleaning
+    df = df.dropna()
+
+    return df
