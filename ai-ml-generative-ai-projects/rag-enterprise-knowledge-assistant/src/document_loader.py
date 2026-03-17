6899e1883c4292f225de181f304e18c4f6cddd07
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 17:47:18 +0530

    Update document_loader.py

--- ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/document_loader.py
+++ ai-ml-generative-ai-projects/rag-enterprise-knowledge-assistant/src/document_loader.py
@@ -1,1 +1,23 @@
+import os
+import re
+from langchain_community.document_loaders import PyPDFLoader
 
+def extract_metadata(filename):
+    match = re.search(r'(\d{4})-(\d{2})', filename)
+    return {
+        "year": int(match.group(1)) if match else None,
+        "month": int(match.group(2)) if match else None
+    }
+
+def load_documents(folder):
+    docs = []
+    for file in os.listdir(folder):
+        if file.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(folder, file))
+            pages = loader.load()
+            metadata = extract_metadata(file)
+
+            for p in pages:
+                p.metadata.update(metadata)
+                docs.append(p)
+    return docs
