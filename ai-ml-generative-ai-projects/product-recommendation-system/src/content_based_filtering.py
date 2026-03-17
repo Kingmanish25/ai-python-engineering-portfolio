7dffcc9823165295b77991dec831ee9a72ce6996
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:21:22 +0530

    Update content_based_filtering.py

--- ai-ml-generative-ai-projects/product-recommendation-system/src/content_based_filtering.py
+++ ai-ml-generative-ai-projects/product-recommendation-system/src/content_based_filtering.py
@@ -1,1 +1,33 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+
+class ContentBasedRecommender:
+
+    def __init__(self, df):
+
+        self.df = df
+
+        self.vectorizer = TfidfVectorizer()
+
+        self.product_features = (
+            df["category"].astype(str) + " " +
+            df["product_color"].astype(str) + " " +
+            df["product_size"].astype(str)
+        )
+
+        self.feature_matrix = self.vectorizer.fit_transform(self.product_features)
+
+        self.similarity = cosine_similarity(self.feature_matrix)
+
+    def recommend(self, product_id, top_n=5):
+
+        idx = self.df[self.df["product_id"] == product_id].index[0]
+
+        scores = list(enumerate(self.similarity[idx]))
+
+        scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+        top_indices = [i[0] for i in scores[1:top_n+1]]
+
+        return self.df.iloc[top_indices][["product_id", "category", "price"]]
