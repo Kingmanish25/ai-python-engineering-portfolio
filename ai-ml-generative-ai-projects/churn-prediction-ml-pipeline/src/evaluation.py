3a7b51397f9f32f227ed2f5c850dfdc0acf449a1
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:56:14 +0530

    Update evaluation.py

--- ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/evaluation.py
+++ ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/evaluation.py
@@ -1,1 +1,15 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+
+def evaluate(model, X_test, y_test):
+
+    y_pred = model.predict(X_test)
+
+    metrics = {
+        "Accuracy": accuracy_score(y_test, y_pred),
+        "Precision": precision_score(y_test, y_pred),
+        "Recall": recall_score(y_test, y_pred),
+        "F1 Score": f1_score(y_test, y_pred)
+    }
+
+    return metrics
