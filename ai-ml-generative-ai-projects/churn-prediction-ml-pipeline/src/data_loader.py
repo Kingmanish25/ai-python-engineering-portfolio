f6314c58e69ee1c34b553878b39cbf10e2afc7bb
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:54:03 +0530

    Update data_loader.py

--- ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/data_loader.py
+++ ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/data_loader.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+
+def load_data(path):
+
+    df = pd.read_csv(path)
+
+    # Remove € and convert to float
+    df["Balance"] = df["Balance"].replace("[€,]", "", regex=True).astype(float)
+
+    # Convert categorical to numeric
+    df["HasCrCard"] = df["HasCrCard"].map({"Yes": 1, "No": 0})
+    df["IsActiveMember"] = df["IsActiveMember"].map({"Yes": 1, "No": 0})
+
+    # Drop duplicates
+    df = df.drop_duplicates()
+
+    return df
