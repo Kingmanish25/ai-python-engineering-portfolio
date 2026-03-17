3f65692f98fc7c46068df63f9d0b41951db84716
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 15:34:06 +0530

    Update sql_generator.py

--- ai-ml-generative-ai-projects/conversational-text-to-sql/src/sql_generator.py
+++ ai-ml-generative-ai-projects/conversational-text-to-sql/src/sql_generator.py
@@ -1,1 +1,13 @@
+def build_prompt(user_query):
+    return f"""
+Convert the following natural language query into SQL.
 
+Rules:
+- Only generate SQL
+- Use table: finance_data
+- Do NOT use DELETE, UPDATE, DROP
+- Only SELECT queries allowed
+
+User Query:
+{user_query}
+"""
