074503888c8fac46328ef8aab78dabda0169eabb
Kingmanish25 <87083437+Kingmanish25@users.noreply.github.com>
2026-03-17 13:55:19 +0530

    Update model_training.py

--- ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/model_training.py
+++ ai-ml-generative-ai-projects/churn-prediction-ml-pipeline/src/model_training.py
@@ -1,1 +1,10 @@
+from sklearn.linear_model import LogisticRegression
 
+
+def train_model(X_train, y_train):
+
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+
+    return model
